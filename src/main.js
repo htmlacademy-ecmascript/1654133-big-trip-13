8e55d78f92b38c90bc29090fc35128cf67c5d431
andrey vdovichenko <andrew.vdovichenko@gmail.com>
2020-12-25 14:16:42 +0500

    Реализовал раскрытие точки маршрута в форму
    Настоящее редактирование
    И в заключение мы реализуем замену точки маршрута на форму редактирования
    и обратно.
    
    Избавьтесь от кода, который сразу отрисовывает в списке форму редактирования.
    Теперь при загрузке приложения должны отображаться только точки маршрута.
    
    В месте, где вы создаёте компонент точки маршрута, а потом его отрисовываете,
    создайте компонент формы редактирования. Теперь у вас есть два компонента:
    точка маршрута и форма редактирования точки маршрута.
    
    Найдите кнопку раскрытия (стрелка вниз) в компоненте точки маршрута и тег
    form в компоненте формы редактирования (не нужно ходить за ними в document,
    используйте метод getElement). Навесьте на них пустые обработчики события
    click и события submit соответственно.
    
    Реализуйте в добавленных обработчиках замену одного компонента на другой
    с помощью replaceChild.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ import Switches from './view/switches';
 import Filters from './view/filters';
 import EventsSortForm from './view/events-sort-form';
 import Events from './view/events';
-import {createAddNewPoint} from './view/add-new-point';
 import EditPoint from './view/edit-point';
 import Point from './view/point';
 import {generateTripPoint} from './mock/point';
-import {renderElement, RenderPosition, renderTemplate} from './utils/utils';
+import {renderElement, RenderPosition} from './utils/utils';
 
 const TRIP_POINTS = 20;
 
@@ -15,6 +14,39 @@ const tripPoints = new Array(TRIP_POINTS).fill().map(generateTripPoint);
 tripPoints.sort(function(a,b) {
   return a.dates[0] - b.dates[0];
 });
+
+const renderPoint = (container, point) => {
+  const pointComponent = new Point(point);
+  const pointEditComponent = new EditPoint(point);
+
+  const replacePointToForm = () => {
+    container.replaceChild(pointEditComponent.getElement(), pointComponent.getElement());
+  }
+
+  const replaceFormToPoint = () => {
+    container.replaceChild(pointComponent.getElement(), pointEditComponent.getElement());
+  }
+
+  pointComponent.getElement()
+    .querySelector(`.event__rollup-btn`)
+    .addEventListener(`click`, () => {
+      replacePointToForm();
+  });
+
+  pointEditComponent.getElement()
+    .querySelector(`form`)
+    .addEventListener(`submit`, () => {
+      replaceFormToPoint();
+    });
+
+  pointEditComponent.getElement()
+    .querySelector(`.event__rollup-btn`)
+    .addEventListener(`click`, () => {
+      replaceFormToPoint();
+    });
+
+  renderElement(container, pointComponent.getElement(), RenderPosition.BEFOREEND);
+}
 
 const tripMainHandler = document.querySelector('.trip-main');
 const tripControlsHandler = tripMainHandler.querySelector('.trip-controls');
@@ -30,5 +62,5 @@ renderElement(tripEventsHandler, new Events().getElement(), RenderPosition.BEFOR
 const tripEventsListHandler = document.querySelector('.trip-events__list');
 
 for (const point of tripPoints) {
-  renderElement(tripEventsListHandler, new Point(point).getElement(), RenderPosition.BEFOREEND);
+  renderPoint(tripEventsListHandler, point);
 }
