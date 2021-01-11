288e384ec97de605c8e175d04e5e3acb27c4d75e
Andrey Vdovichenko <andrew.vdovichenko@gmail.com>
2021-01-11 17:52:15 +0500

    Создали абстрактный класс Smart
    - абстрактный метод restoreHandlers;
    - метод updateElement;
    - метод updateData.

---
+++ src/view/smart.js
@@ -0,0 +1,35 @@
+import Abstract from './abstract';
+
+export default class Smart extends Abstract {
+  constructor() {
+    super();
+    this._data = {};
+  }
+
+  restoreHandlers() {
+    throw new Error(`Abstract method not implemented: restoreHandlers.`);
+  }
+
+  updateElement() {
+    let prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.getElement();
+    parent.replaceChild(newElement, prevElement);
+  }
+
+  updateDate(update, justDataUpdating) {
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign({}, this._data, update);
+
+    if (justDataUpdating) {
+      return;
+    }
+
+    this.updateElement();
+  }
+}
